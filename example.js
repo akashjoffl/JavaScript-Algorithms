07ce1f817a72a0117a0dcfad37cbb002496ace3c
Akash Jeganath <akashjoffl@icloud.com>
2020-05-25 21:47:45 +0530

    140. 4. Solution: Fizz Buzz

--- example.js
+++ example.js
@@ -1,8 +1,44 @@
-function convertToFarenheit(celcius) {
-    return celcius * 9 / 5 + 32;  
+function fizzBuzz(num){
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "fizzbuzz";
+    }
+    else if ( num % 3 === 0) {
+        return "fizz";
+    }
+    else if (num % 5 === 0) {
+        return "buzz";
+    }
+    return "";
+}
+ 
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(10));
+console.log(fizzBuzz(8));
+
+// Write a function that if a number 3 or greater and is divisible by 3
+// returns 'fizz', by 5 returns 'buzz', by both returns 'fizzbuzz', or none ''.
+
+// 3 return 'fizz'
+// 5 return 'buzz'
+// 4 return ''
+// 15 return 'fizzbuzz'
+
+
+function fizzBuzz2(num) {
+    let word = "";
+
+    if (num % 3 === 0){
+        word = word.concat("fizz");
+    }
+
+    if (num % 5 === 0) {
+        word = word.concat("buzz");
+    }
+    return word;
 }
 
-console.log(convertToFarenheit(30));
-console.log(convertToFarenheit(-30));
-
-// Write a function that converts Celcius to Fahrenheit
+console.log(fizzBuzz2(3));
+console.log(fizzBuzz2(15));
+console.log(fizzBuzz2(10));
+console.log(fizzBuzz2(8));
