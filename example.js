29fae4879c7c1c383c2cbbee4a20429cac26a6d2
Akash Jeganath <akashjoffl@icloud.com>
2020-05-26 22:14:31 +0530

    142. 5. Solution: Factorial

--- example.js
+++ example.js
@@ -1,44 +1,14 @@
-function fizzBuzz(num){
-    if (num % 3 === 0 && num % 5 === 0) {
-        return "fizzbuzz";
-    }
-    else if ( num % 3 === 0) {
-        return "fizz";
-    }
-    else if (num % 5 === 0) {
-        return "buzz";
-    }
-    return "";
-}
- 
-console.log(fizzBuzz(3));
-console.log(fizzBuzz(15));
-console.log(fizzBuzz(10));
-console.log(fizzBuzz(8));
+function factorializeNumber(num) {
+    let product = 1;
 
-// Write a function that if a number 3 or greater and is divisible by 3
-// returns 'fizz', by 5 returns 'buzz', by both returns 'fizzbuzz', or none ''.
-
-// 3 return 'fizz'
-// 5 return 'buzz'
-// 4 return ''
-// 15 return 'fizzbuzz'
-
-
-function fizzBuzz2(num) {
-    let word = "";
-
-    if (num % 3 === 0){
-        word = word.concat("fizz");
+    for (let i = 1; i <= num; i++){
+        console.log(i);
+        console.log(product);
+        product = product * i;
     }
 
-    if (num % 5 === 0) {
-        word = word.concat("buzz");
-    }
-    return word;
+    return product;
 }
 
-console.log(fizzBuzz2(3));
-console.log(fizzBuzz2(15));
-console.log(fizzBuzz2(10));
-console.log(fizzBuzz2(8));
+console.log(factorializeNumber(5));
+//console.log(1 * 2 * 3 * 4 * 5);
