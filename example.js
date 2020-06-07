7f678c6daedd81318564b7ca4bf1b3e16a986309
Akash Jeganath <akashjoffl@icloud.com>
2020-06-07 22:30:26 +0530

    154. 11. Solution: Is Digit

--- example.js
+++ example.js
@@ -1,8 +1,11 @@
-function repeatString(str, num) {
-    return str.repeat(num);
+function isDigit(str) {
+    const digitArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    return digitArray.includes(str);
 }
 
-console.log(repeatString('codinggod', 2));
+console.log(isDigit('2'));
+console.log(isDigit('d'))
 
 
-// Repeat string
+// isDigit
