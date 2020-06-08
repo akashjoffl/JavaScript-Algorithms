c8dc890559aff2df9c26fa9aade735b52247ec91
Akash Jeganath <akashjoffl@icloud.com>
2020-06-08 12:30:32 +0530

    156. 12: Solution: Longest Word

--- example.js
+++ example.js
@@ -1,11 +1,15 @@
-function isDigit(str) {
-    const digitArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+function longestWord(str) {
+    let filteredArray = str.replace(/[\W_]/g, ' ').split(' ');
+    let largest = "";
 
-    return digitArray.includes(str);
+    filteredArray.forEach((word) => {
+        largest = word.length > largest.length ? word : largest;
+    });
+
+    return largest;
 }
 
-console.log(isDigit('2'));
-console.log(isDigit('d'))
+console.log(longestWord('Ready, steady, goooooood!, [df]'));
 
 
-// isDigit
+// longestWord
