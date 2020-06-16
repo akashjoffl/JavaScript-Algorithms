464026a0e8ab4336057f771558115c78a5330067
Akash Jeganath <akashjoffl@icloud.com>
2020-06-16 21:24:52 +0530

    158. 13. Solution: Valid Time

--- example.js
+++ example.js
@@ -1,15 +1,19 @@
-function longestWord(str) {
-    let filteredArray = str.replace(/[\W_]/g, ' ').split(' ');
-    let largest = "";
+function isValidTime(time) {
+    let timeArray = time.split(':');
 
-    filteredArray.forEach((word) => {
-        largest = word.length > largest.length ? word : largest;
-    });
+    if (timeArray[0] > 23) {
+        return false;
+    } else if (timeArray[1] > 59) {
+        return false;
+    }
 
-    return largest;
+    return true;
 }
 
-console.log(longestWord('Ready, steady, goooooood!, [df]'));
+console.log(isValidTime('24:00'));
+console.log(isValidTime('23:59'));
+console.log(isValidTime('12:40'));
+console.log(isValidTime('23:69'));
 
 
-// longestWord
+// validTime
